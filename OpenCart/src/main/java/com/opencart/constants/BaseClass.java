ee1d8bf66eaac512bf1b7ca53999ebc4bb4c10aa
Vasantha <Vasantha@DESKTOP-0JHIS2I>
2021-05-19 18:01:17 +0530

    add log.propertiesfile and changed the screenshot functionality

--- OpenCart/src/main/java/com/opencart/constants/BaseClass.java
+++ OpenCart/src/main/java/com/opencart/constants/BaseClass.java
@@ -8,7 +8,6 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Date;
 import java.util.Properties;
-
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 import org.codehaus.plexus.util.FileUtils;
@@ -18,28 +17,24 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Parameters;
-import org.testng.annotations.Test;
 
 public class BaseClass {
 		
 		public static WebDriver driver;
 		public static Properties prop;
 		public  static WebDriverWait wait;
-		final static Logger logger = Logger.getLogger(BaseClass.class);
+		 static Logger log = Logger.getLogger(BaseClass.class);
 		
 		/**
 	     *  Load Properties File 
 	     */
 		public BaseClass() {
 				prop = new Properties();
-				FileInputStream file,file1;
+				PropertyConfigurator.configure("log4j.properties");	
+				FileInputStream file;
 				try {
 					file = new FileInputStream("C:\\Users\\user\\workspace\\OpenCart\\src\\main\\java\\com\\opencart\\config\\config.properties");
-					file1= new FileInputStream("C:\\Users\\user\\workspace\\OpenCart\\src\\main\\java\\com\\opencart\\config\\log4j.properties");
 					prop.load(file);
-					prop.load(file1);
 				} catch (FileNotFoundException e) {
 					e.printStackTrace();
 				} catch (IOException e) {
@@ -54,13 +49,13 @@ public class BaseClass {
 			PropertyConfigurator.configure("log4j.properties");
 			//System.out.println("Script running by google chrome browser");
 			System.setProperty("webdriver.chrome.driver", "C:\\Users\\user\\workspace\\OpenCart\\Drivers\\chromedriver.exe");
-			logger.debug("Debug this path for chrome path issue");
+			log.debug("Debug this path for chrome path issue");
 			WebDriver driver = new ChromeDriver();
-			logger.info("Chrome driver is up and running");
+			log.info("Chrome driver is up and running");
 			driver.manage().window().maximize();
 			driver.manage().deleteAllCookies();
 			driver.get(prop.getProperty("url"));
-			logger.warn("Url is not loaded properly");
+			log.warn("Url is not loaded properly");
 			return driver;
 		}
 		/**
@@ -108,12 +103,15 @@ public class BaseClass {
 		/**
 	     *  Take Screenshots 
 	     */
-		public static void takeSnapShot(WebDriver webdriver,String Filename) throws Exception{
+		public static void takeSnapShot(WebDriver webdriver,String screenshotName) throws Exception{
 
 			Date d=new Date();
-			String FileName = d.toString().replace(":", "_").replace(" ", "_") + ".png";
+			String dateString = d.toString().replace(":", "_").replace(" ", "_");
+			
+			String destPath ="C:\\Users\\user\\workspace\\YLigthingDemo\\screenshot\\" + dateString + screenshotName + ".png";
+
 			 File scr = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-			   File dest = new File("screenshot/" + FileName);
-		        FileUtils.copyFile(scr, dest); 
+				File DestFile = new File(destPath);
+		        FileUtils.copyFile(scr, DestFile); 
 		     		}
 		}
